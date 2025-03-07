a6f0e889db1707f9e86b33d1967ec0764c748856
Storius <hieupt1003@gmail.com>
2025-03-07 16:26:44 +0700

    Update register (using callbacks)

---
+++ src/controllers/authController.js
@@ -0,0 +1,78 @@
+const connection = require('../config/dbConfig');
+const bcrypt = require('bcrypt');
+
+const getLoginPage = (req, res) => {
+    // Flash này chỉ có key, không có thông báo nên khi vào trang login lần đầu thì không có thông báo nào
+    res.render('login.ejs', { messages: req.flash('error') });
+}
+
+const postLoginPage = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        req.flash('error', 'Must provide username and password');
+        // Thông báo 'Must provide username and password' được lưu vào key 'error' trong session
+        // và sẽ được sử dụng cho request tiếp theo (ở đây là request GET /login)
+        return res.redirect('/login');
+    }
+
+    
+    // const [listUsers, fields] = await connection.query('SELECT * FROM users WHERE username = ?', [username])
+    
+    connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, rows) => {
+        if (err) {
+            console.error(err);
+            req.flash('error', 'Database error');
+            return res.redirect('/login');
+        }
+
+        if (rows.length !== 1 || !await bcrypt.compare(password, rows[0].hash)) {
+            req.flash('error', 'Invalid username and/or password');
+            return res.redirect('/login');
+        }
+
+        req.session.user_id = rows[0].id;
+        res.redirect('/');
+    });
+}
+
+const getRegisterPage = (req, res) => {
+    res.render('register.ejs', { messages: req.flash('error') });
+}
+
+const postRegisterPage = (req, res) => {
+    const { username, password, confirmation } = req.body;
+    if (!username || !password || !confirmation) {
+        req.flash('error', 'Must provide username and password and confirmation');
+        return res.redirect('/register');
+    }
+
+    connection.query("SELECT username FROM users", (err, listUsers) => {
+        if (err) {
+            console.error(err);
+            req.flash('error', 'Database error');
+            return res.redirect('/register');
+        }
+        
+        if (username in listUsers) {
+            req.flash('error', 'Username already exists');
+            return res.redirect('/register');
+        }
+    })
+
+    if (password != confirmation) {
+        req.flash('error', 'Confirmation does not match password');
+        return res.redirect('/register');
+    }
+    
+    connection.query("INSERT INTO users (username, hash) VALUES (?, ?)", 
+        [username, bcrypt.hashSync(password, 10)], 
+        (err) => {
+            if (err) {
+                console.error(err);
+                req.flash('error', 'Database error');
+                return res.redirect('/');
+            }
+        })
+}
+
+module.exports = { getLoginPage, postLoginPage, getRegisterPage, postRegisterPage };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,56 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const { loginRequired } = require('../utils/helpers');
-const connection = require('../config/dbConfig');
-
+const { getLoginPage, 
+        postLoginPage, 
+        getRegisterPage, 
+        postRegisterPage } = require('../controllers/authController');
 
 router.get('/', loginRequired, (req, res) => {
     res.send("TODO");
 });
 
 router.route('/login')
-    .get((req, res) => {
-        // Flash này chỉ có key, không có thông báo nên khi vào trang login lần đầu thì không có thông báo nào
-        res.render('login.ejs', { messages: req.flash('error') });
-    })
-    .post((req, res) => {
-        const { username, password } = req.body;
-        if (!username || !password) {
-            req.flash('error', 'Must provide username and password');
-            // Thông báo 'Must provide username and password' được lưu vào key 'error' trong session
-            // và sẽ được sử dụng cho request tiếp theo (ở đây là request GET /login)
-            return res.redirect('/login');
-        }
+    .get(getLoginPage)
+    .post(postLoginPage);
 
-        connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, rows) => {
-            if (err) {
-                console.error(err);
-                req.flash('error', 'Database error');
-                return res.redirect('/login');
-            }
-
-            if (rows.length !== 1 || !await bcrypt.compare(password, rows[0].hash)) {
-                req.flash('error', 'Invalid username and/or password');
-                return res.redirect('/login');
-            }
-
-            req.session.user_id = rows[0].id;
-            res.redirect('/');
-        });
-    });
+router.route('/register')
+    .get(getRegisterPage)
+    .post(postRegisterPage);
 
 router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 });
 
-router.route('/register')
-    .get((req, res) => {
-        res.render('register.ejs', { messages: req.flash('error') });
-    })
-    .post((req, res) => {
-        res.send("TODO");
-    });
+
 
 module.exports = router;
